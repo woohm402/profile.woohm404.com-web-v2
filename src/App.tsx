40654e8fd5f6428bca470162d8d48998586b3940
woohm402 <woohm404@gmail.com>
2021-07-18 18:03:17 +0900

    react-ga 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 
 import styles from './App.module.scss';
 
+ReactGA.initialize('G-S5E2X3FX3D');
+
 const App: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location.pathname, location.search]);
+
   return (
     <div className={styles.app}>
       <Home />
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
